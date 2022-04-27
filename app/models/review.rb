ec1271a70786c24f8114e13558254b9f62e14cd1
Vinicius Vinci <vvinci@protonmail.com>
2022-04-26 23:32:21 -0300

    models created, validation created and reference restaurant table added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :content, presence: true, length: { minimum: 10 }
+  validates :content, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: -1, less_than: 6 }
 end
